3fd20816b00069d60720f146f6a0ace935061d31
Juan Solano <Juan Solano@DESKTOP-MFRIJCF>
2021-10-13 11:13:49 -0500

    Se agrega sección de productos

---
+++ ciclo3back/src/main/java/co/edu/unbosque/ciclo3back/dao/ProductosDAO.java
@@ -0,0 +1,9 @@
+package co.edu.unbosque.ciclo3back.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.unbosque.ciclo3back.model.Productos;
+
+public interface ProductosDAO extends JpaRepository<Productos, Long>{
+	
+}
